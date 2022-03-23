5952499f49a90ba400ab012d4d52a68158cf6d35
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-03-23 10:44:19 +0100

    Add admin

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const { Configuration, OpenAIApi } = require("openai");
+const admin = require("firebase-admin");
 const db = admin.firestore();
 
 exports.testFunction = functions
