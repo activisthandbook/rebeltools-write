f963d83788a0086ac6c35d581014a15edeeeabc0
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-03-23 11:35:19 +0100

    Save result to firestore

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -21,13 +21,25 @@ export default defineComponent({
         this.$store.state.firebase.functions,
         "testFunction"
       );
-      testFunction()
+      testFunction({ id: this.makeid() })
         .then((result) => {
           console.log(result.data);
         })
         .catch((error) => {
           console.log(error);
         });
+    },
+    makeid() {
+      var result = "";
+      var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      var charactersLength = characters.length;
+      for (var i = 0; i < 30; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
     },
   },
 });
